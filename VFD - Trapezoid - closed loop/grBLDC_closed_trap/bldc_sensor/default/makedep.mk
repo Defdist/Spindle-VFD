4c95aaae540c2196167aed3dc4604d16676e8ea3
doppelhub <buhtig.elt@xoxy.net>
2020-09-11 02:06:30 -0400

    Initital closed loop BLDC VFD commit
    This code compiles without error.  It doesn't work, but it compiles.

---
+++ VFD - Trapezoid - closed loop/grBLDC_closed_trap/bldc_sensor/default/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ascii.c
+
+main.c
+
+mc_control.c
+
+mc_drv.c
+
+mc_interface.c
+
+ushell_task.c
+
+uart\uart_lib.c
+
---
+++ VFD - Trapezoid - closed loop/grBLDC_closed_trap/bldc_sensor/default/mc_interface.d
@@ -0,0 +1,60 @@
+mc_interface.d mc_interface.o mc_interface.o: .././mc_interface.c \
+  ../conf/config.h ../compiler.h ../compiler.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\Atmel\ATmega_DFP\1.4.331\include/avr/iom32m1.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/power.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  ../conf/config_motor.h .././mc_interface.h .././mc_control.h \
+  .././mc_drv.h
+
+../conf/config.h:
+
+../compiler.h:
+
+../compiler.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\Atmel\ATmega_DFP\1.4.331\include/avr/iom32m1.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/power.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+../conf/config_motor.h:
+
+.././mc_interface.h:
+
+.././mc_control.h:
+
+.././mc_drv.h:
